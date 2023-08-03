6e11bd914e4d29d1b392f4cd6cb4ee3dc9e7b527
fuserojeshpradhananga <rojesh.pradhananga@fusemachines.com>
2023-08-03 11:54:40 +0545

    added second test case

--- args.py
+++ args.py
@@ -4,3 +4,6 @@ def sum_of_numbers(*args):
 # Test case 1: Sum of 1, 2, 3, 4, and 5
 print(sum_of_numbers(1, 2, 3, 4, 5))
 
+
+# Test case 2: Sum of 100, 200, 300, 400, and 500
+nums = [100, 200, 300, 400, 500]

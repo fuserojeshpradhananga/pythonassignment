18919f51dfc7696cd43abfc1bea987fa9edf1562
fuserojeshpradhananga <rojesh.pradhananga@fusemachines.com>
2023-08-05 13:59:44 +0545

    Zero Division error Exception Handling0

---
+++ zerodivisionerror.py
@@ -0,0 +1,16 @@
+def perform_division(num1, num2):
+    try:
+        result = num1 / num2
+        return result
+    except ZeroDivisionError:
+        raise ValueError("Error: Division by zero is not allowed.")
+
+if __name__ == "__main__":
+    try:
+        num1 = int(input("Enter the first number: "))
+        num2 = int(input("Enter the second number: "))
+
+        result = perform_division(num1, num2)
+        print(f"The result of {num1} / {num2} is {result:.2f}")
+    except ValueError as e:
+        print(e)
